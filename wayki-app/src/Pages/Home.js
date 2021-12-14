eee86b3b50b286c4d82baa80e636ffcc856bc592
benjaminhonorio <benjaminhonorio.12@gmail.com>
2021-12-13 20:37:56 -0500

    Refactor homepage and footer markup to components
    Also add the logo to Hero like section of homepage

--- wayki-app/src/Pages/Home.js
+++ wayki-app/src/Pages/Home.js
@@ -1,53 +1,66 @@
-import { Row, Card, Col, Button } from "react-bootstrap";
+import {
+  Row,
+  Card,
+  Col,
+  Form,
+  Button,
+  Image,
+  Container,
+} from "react-bootstrap";
 import pic from "../dog.jpg";
+import WaykiLogo from "../logoH.png";
+import Map from "../Components/Map";
+import credentials from "../Components/credentials";
 
 export default function Home() {
   const posts = Array.from(Array(12).keys());
   return (
-    <div className="text-center container-fluid">
-      <div className="row">
-        <div className="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center">
-          <h1 className="display-1">Wayki</h1>
-          <h2 className="display-6">Search and find missing pets</h2>
-          <form className="w-75">
-            <div className="input-group mb-3">
-              <input
+    <Container className="text-center" fluid>
+      <Row>
+        <Col
+          style={{ marginTop: "-56px" }}
+          className="d-flex flex-column justify-content-center align-items-center vh-100"
+        >
+          <Col xs={10} sm={8}>
+            <Image src={WaykiLogo} alt="logo" fluid />
+          </Col>
+          <p className="h4 fw-light">
+            Search and find missing pets around your area
+          </p>
+          <Form className="col-12 col-sm-10">
+            <Form.Group className="input-group mb-3">
+              <Form.Control
                 type="text"
                 className="form-control"
                 placeholder="Search by name, location, color, etc"
                 aria-label="Search bar"
                 aria-describedby="basic-addon2"
               />
-              <button className="btn btn-outline-secondary" type="button">
-                Search
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="col-12 col-md-6 vh-100">
-          <iframe
-            title="map"
-            width="100%"
-            height="94%"
-            id="gmap_canvas"
-            src="https://maps.google.com/maps?q=Lima&t=&z=13&ie=UTF8&iwloc=&output=embed"
-            scrolling="no"
-          ></iframe>
-        </div>
-      </div>
+            </Form.Group>
+            <Button variant="primary">Search now</Button>
+          </Form>
+        </Col>
+        <Col className="col-12 col-md-6">
+          <Map
+            googleMapURL={credentials.mapsKey}
+            containerElement={<div style={{ height: "94vh" }} />}
+            mapElement={<div style={{ height: "100%" }} />}
+            loadingElement={<div style={{ height: "100%" }} />}
+          />
+        </Col>
+      </Row>
       <Row className="row d-flex justify-content-center py-3">
-        <h2>All missing pets</h2>
+        <h2>Publications</h2>
         <Row className="d-flex justify-content-center row-cols-auto">
-          {posts.map(function (post) {
+          {posts.map(function (_, i) {
             return (
-              <Col className="py-2">
+              <Col key={i} className="py-2">
                 <Card style={{ width: "18rem" }}>
                   <Card.Img variant="top" src={pic} />
                   <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Post Title</Card.Title>
                     <Card.Text>
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
+                      Hace 10 minutos
                     </Card.Text>
                     <Button variant="primary">Go somewhere</Button>
                   </Card.Body>
@@ -59,9 +72,9 @@ export default function Home() {
       </Row>
       <Row className="container mx-auto py-5">
         <Col>
-          <h2>About Wayki</h2>
-          <div className="row">
-            <div className="col">
+          <h2 id="about">About Wayki</h2>
+          <Row>
+            <Col>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
               vitae quam a lacus blandit interdum ac congue nulla. Curabitur
               luctus laoreet congue. Integer at nisi malesuada, lacinia massa a,
@@ -72,10 +85,10 @@ export default function Home() {
               vulputate. Morbi libero tortor, scelerisque non nunc ac,
               pellentesque varius metus. Praesent eu velit turpis. Pellentesque
               rutrum purus eget massa condimentum venenatis.
-            </div>
-          </div>
+            </Col>
+          </Row>
         </Col>
       </Row>
-    </div>
+    </Container>
   );
 }
