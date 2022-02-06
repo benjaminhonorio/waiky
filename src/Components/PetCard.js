979f89244d31376356065337477db9f1c8866e02
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-02-06 11:41:21 -0500

    Resolve conflic in petcard

--- src/Components/PetCard.js
+++ src/Components/PetCard.js
@@ -5,41 +5,40 @@ import { es } from "date-fns/locale";
 
 export default function PetCard({ id, photos, title, date }) {
   return (
-    <Col key={id} className="py-2">
-      <div
-        className="d-flex flex-row flex-md-column"
+    <Col
+      key={id}
+      className="py-2"
+      style={{
+        wordWrap: "break-word",
+        backgroundColor: "#fff",
+        backgroundClip: "border-box",
+        border: "1px solid rgba(0,0,0,.125)",
+        borderRadius: "0.25rem",
+      }}
+    >
+      <Card.Img
         style={{
-          wordWrap: "break-word",
-          backgroundColor: "#fff",
-          backgroundClip: "border-box",
-          border: "1px solid rgba(0,0,0,.125)",
-          borderRadius: "0.25rem",
+          minWidth: "50%",
+          height: "300px",
+          objectFit: "cover",
         }}
-      >
-        <Card.Img
-          style={{
-            minWidth: "50%",
-            height: "300px",
-            objectFit: "cover",
-          }}
-          variant="top"
-          src={photos[0]}
-        />
-        <Card.Body className="d-flex flex-column justify-content-center">
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>
-            Hace{" "}
-            {date
-              ? formatDistance(new Date(), new Date(date), {
-                  locale: es,
-                })
-              : ""}
-          </Card.Text>
-          <Button as={Link} to={`/post/${id}`} variant="primary">
-            Ver Más
-          </Button>
-        </Card.Body>
-      </div>
+        variant="top"
+        src={photos[0]} //later change to mainphoto
+      />
+      <Card.Body className="d-flex flex-column justify-content-center">
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          Hace{" "}
+          {date
+            ? formatDistance(new Date(), new Date(date), {
+                locale: es,
+              })
+            : ""}
+        </Card.Text>
+        <Button as={Link} to={`/post/${id}`} variant="primary">
+          Ver Más
+        </Button>
+      </Card.Body>
     </Col>
   );
 }
