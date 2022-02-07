b82a43d8d42905268a14a29c55f23083c0f7f78f
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-02-07 15:44:13 -0500

    Comment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+// Comment
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
